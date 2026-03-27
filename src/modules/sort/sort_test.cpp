e17cf53598f2342f6b35238a53278f0359144988
Andre Mochammad Satrio <andre.satrio@ms.mii.co.id>
2026-03-27 15:12:23 +0700

    fix test case

--- src/modules/sort/sort_test.cpp
+++ src/modules/sort/sort_test.cpp
@@ -1,9 +1,15 @@
 #include "sort.h"
 #include <memory>
+#include <assert.h>
 
 int main(){
     std::vector<int> vec = {2, 2, 5 ,1, 6, 3, 7, 0, 9};
+    std::vector<int> sorted = {0, 1, 2, 2, 3, 5, 6, 7, 9};
+    
     std::unique_ptr<Sort> sort = std::make_unique<BubbleSort>();
     sort->execute(vec);
-    sort->print(vec);
+    assert(vec == sorted);
+    std::cout << "[PASS] buble sort check" << std::endl;
+
+
 }
